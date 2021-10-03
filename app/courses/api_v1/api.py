01973f782d6ac955ac9112bd58c4069cea471c97
agustin <agustin.contini@despegar.com>
2021-10-03 14:54:44 -0300

    added user and endpoint for user interaction + re organize modules

--- app/courses/api_v1/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from app.courses.api_v1 import resources
-
-
-course_router = APIRouter()
-course_router.include_router(resources.router_v1, prefix="/courses", tags=["courses"])
