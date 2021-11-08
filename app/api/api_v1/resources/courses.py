c1834cd372079f7dde44e83286a39d6c1c884c94
agustin <agscontini@gmail.com>
2021-11-07 21:51:29 -0300

    added pagination for courses

--- app/api/api_v1/resources/courses.py
+++ app/api/api_v1/resources/courses.py
@@ -19,12 +19,15 @@ async def search_courses(
     *,
     keyword: Optional[str] = Query(None, min_length=3, example="java"),
     max_results: Optional[int] = 10,
+    page_size: Optional[int] = 10,
+    page: Optional[int] = 1,
     db: Session = Depends(deps.get_db),
 ) -> dict:
     """
     Search for courses based on hashtags keyword
     """
-    courses = crud.course.get_multi(db=db, limit=max_results)
+    await pagination_validator(page=page, page_size=page_size)
+    courses = crud.course.get_multi(db=db, limit=page_size, offset=(page - 1) * page_size)
     if not keyword:
         return {"results": courses}
 
@@ -128,3 +131,11 @@ async def update_course(course_id: int, course_update_rq: CourseUpdateRq,
 
     return course_updated
 
+
+async def pagination_validator(page, page_size):
+    if page < 1:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Page value must be at least 1")
+    if page_size < 1:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Page size must be at least 1")
