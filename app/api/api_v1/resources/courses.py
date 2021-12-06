e86cfc1a3aa2eeb0e922a87ddaac3b4347344fd9
agustin <agscontini@gmail.com>
2021-12-06 01:37:13 -0300

    added user subscription validation before allowing user to enroll in course

--- app/api/api_v1/resources/courses.py
+++ app/api/api_v1/resources/courses.py
@@ -13,6 +13,8 @@ from app.schemas.enroll_course import EnrollCourse
 from app.models.collaborator import Collaborator as CollaboratorDb
 from app.schemas.collaborator import Collaborator
 from app.services import subscription_service
+from app.models.subscription import Subscription
+from app.schemas.subscription import SubscriptionTitle
 
 logger = logging.getLogger(__name__)
 
@@ -77,6 +79,17 @@ async def get(course_id: int, db: Session = Depends(deps.get_db), ):
     return course
 
 
+def validate_user_subscription_against_course_subscription(user_subscription: Subscription,
+                                                           course_subscription_required: Subscription):
+    if course_subscription_required.title.lower() == SubscriptionTitle.free.lower():
+        return True
+    elif course_subscription_required.title.lower() == SubscriptionTitle.gold.lower():
+        return True if (user_subscription.title.lower() == SubscriptionTitle.gold.lower()
+                        or user_subscription.title.lower() == SubscriptionTitle.premium.lower()) else False
+    elif course_subscription_required.title.lower() == SubscriptionTitle.premium.lower():
+        return True if user_subscription.title.lower() == SubscriptionTitle.premium.lower() else False
+
+
 @router_v1.post("/{course_id}/registration", status_code=status.HTTP_200_OK, response_model=EnrollCourse)
 async def course_registration(course_id: int, course_registration_rq: CourseRegistration,
                               db: Session = Depends(deps.get_db),) -> dict:
@@ -91,6 +104,17 @@ async def course_registration(course_id: int, course_registration_rq: CourseRegi
     if any(filter(lambda enroll_course: str(course_id) in str(enroll_course.course_id), user.enroll_courses)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"User {user_id} is already register in course {course_id}")
+
+    user_current_subscription = subscription_service.get_current_subscription(user.subscriptions)
+    subscription_validated = validate_user_subscription_against_course_subscription(
+        user_subscription=user_current_subscription.subscription,
+        course_subscription_required=course.subscription_required
+    )
+    if not subscription_validated:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"The course with id {course_id} requires a {course.subscription_required.title} subscription and"
+                   f" user {user_id} has a {user_current_subscription.subscription.title} subscription")
 
     student_enroll_course = crud.enroll_course.create(db=db, obj_in=EnrollCourseDb(user_id=user_id, course_id=course.id))
 
